4bcf456db4288cb8b0726809334ae0103820f0d6
Cassandra Blondez <[cassandra.blondez@gmail.com](mailto:cassandra.blondez@gmail.com)>
2023-04-03 23:01:16 +0200

    get colors

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -1,17 +1,36 @@
+import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
-function Note({ noteId, title, content, colorId }) {
+import expressAPI from "../services/expressAPI";
+
+function Note({ title, content, colorId }) {
+  const [color, setColor] = useState();
+
+  useEffect(() => {
+    expressAPI
+      .get(`/colors/${colorId}`)
+      .then((res) => setColor(res.data))
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
-    <div className={`bg-${colorId}`}>
-      <h2>{title}</h2>
-      <p>{content}</p>
-      <p>{noteId}</p>
+    <div>
+      {color && (
+        <div
+          className="mx-5 rounded-r-lg rounded-tl-lg py-4 px-6 text-main text-sm max-w-[45%]"
+          style={{ backgroundColor: color.name }}
+        >
+          <h2 className="font-semibold mb-3">{title}</h2>
+          <p>{content}</p>
+        </div>
+      )}
     </div>
   );
 }
 
 Note.propTypes = {
-  noteId: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
   colorId: PropTypes.number.isRequired,
--- frontend/src/pages/Accueil.jsx
+++ frontend/src/pages/Accueil.jsx
@@ -4,12 +4,15 @@ import Categories from "../components/Accueil/Categories";
 import SearchBar from "../components/Accueil/SearchBar";
 import RoundButton from "../components/Buttons/RoundButton";
 
+import { useCurrentUserContext } from "../contexts/CurrentUserContext";
+
 import bullets from "../assets/images/bullets.svg";
 import create from "../assets/icons/addition_white.svg";
 import NotesList from "../components/NotesList";
 
 function Accueil() {
   const navigate = useNavigate();
+  const { user } = useCurrentUserContext();
 
   const handleCreateNote = (e) => {
     e.preventDefault();
@@ -20,17 +23,19 @@ function Accueil() {
     <div>
       <SearchBar />
       <Categories />
-      <div className="flex flex-col justify-center items-center text-xl">
-        <img src={bullets} alt="bullet-color" className="w-64 mb-6" />
-        <h1 className="font-lilita text-main text-center w-1/2 mb-6">
-          Vous n'avez pas encore de note, créez en une !
-        </h1>
-        <RoundButton
-          src={create}
-          alt="add note"
-          handleClick={handleCreateNote}
-        />
-      </div>
+      {!user && (
+        <div className="flex flex-col justify-center items-center text-xl">
+          <img src={bullets} alt="bullet-color" className="w-64 mb-6" />
+          <h1 className="font-lilita text-main text-center w-1/2 mb-6">
+            Vous n'avez pas encore de note, créez en une !
+          </h1>
+          <RoundButton
+            src={create}
+            alt="add note"
+            handleClick={handleCreateNote}
+          />
+        </div>
+      )}
       <NotesList />
     </div>
   );
