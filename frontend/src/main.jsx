4543b9ad1ac280e337fdb52cbe6bf982b783749d
Cassandra Blondez <[cassandra.blondez@gmail.com](mailto:cassandra.blondez@gmail.com)>
2023-04-03 11:18:01 +0200

    add user context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import CurrentUserContextProvider from "./contexts/CurrentUserContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <CurrentUserContextProvider>
+      <App />
+    </CurrentUserContextProvider>
   </React.StrictMode>
 );
